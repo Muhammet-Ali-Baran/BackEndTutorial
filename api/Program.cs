0ce96ed04480fd5530fb52f952f10c6dff0a5338
Muhammet Ali Baran <alibaran0415@gmail.com>
2025-07-20 01:03:33 +0300

    Add Interfaces and Repository, update StockController and Program

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("BackEndTutorial"));
 });
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
