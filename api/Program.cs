6ed64bd7a629a8055f2bd234cebfd9404d610a1d
Muhammet Ali Baran <alibaran0415@gmail.com>
2025-09-03 21:04:18 +0300

    Token Service added

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -53,6 +54,7 @@ options.TokenValidationParameters = new TokenValidationParameters
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
